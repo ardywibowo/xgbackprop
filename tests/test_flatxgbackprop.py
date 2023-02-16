436ec3d5a80760776d7d143b2f4360091800b03d
Randy Ardywibowo <ardywibowo.randy@gmail.com>
2023-02-15 17:12:25 -0800

    Implemented FlatXGBackprop (not correct)

---
+++ tests/test_flatxgbackprop.py
@@ -0,0 +1,59 @@
+import json
+import os
+import sys
+
+import numpy as np
+import torch
+import ubjson
+from matplotlib import pyplot as plt
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from xgbackprop.xgbackprop import FlatXGBackpropLayer
+
+
+def test_xgbackprop():
+    # load model from file
+    model = '/Users/randyardywibowo/Github/xgbackprop/models/model.json'
+    
+    if model.endswith("json"):
+        # use json format
+        with open(model, "r") as fd:
+            model = json.load(fd)
+    elif model.endswith("ubj"):
+        if ubjson is None:
+            raise ImportError("ubjson is not installed.")
+        # use ubjson format
+        with open(model, "rb") as bfd:
+            model = ubjson.load(bfd)
+    else:
+        raise ValueError(
+            "Unexpected file extension. Supported file extension are json and ubj."
+        )
+    
+    model = FlatXGBackpropLayer(model)
+    # model = torch.jit.script(model)
+    # model = torch.compile(model)
+    input = torch.nn.Embedding(2, 8)
+    optimizer = torch.optim.Adam(input.parameters())
+    
+    outputs = []
+    for i in range(1000):
+        encoder_output = input(torch.tensor([0, 1]))
+        output = model(encoder_output)
+        if (i // 100) % 2 == 0:
+            print("Target: -10")
+            loss = torch.square(output)
+            outputs.append(output.detach().numpy())
+        else:
+            print("Target: +10")
+            loss = torch.square(output - 1)
+            outputs.append(output.detach().numpy())
+        
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        
+    outputs = np.array(outputs)
+    plt.plot(outputs)
+    
+test_xgbackprop()
--- xgbackprop/xgbackprop.py
+++ xgbackprop/xgbackprop.py
@@ -276,3 +276,190 @@ class XGBackpropLayer(nn.Module):
         preds = torch.stack([tree(input) for tree in self.trees], dim=-1)
         out = torch.sum(preds, dim=-1) + self.base_score
         return out
+
+class FlatXGBackpropLayer(nn.Module):
+    """Gradient boosted tree model."""
+
+    def __init__(self, model: dict, trainable: bool = True) -> None:
+        """Construct the Model from a JSON object.
+        parameters
+        ----------
+         model : A dictionary loaded by json representing a XGBoost boosted tree model.
+        """
+        super().__init__()
+        
+        # Basic properties of a model
+        self.learner_model_shape: ParamT = model["learner"]["learner_model_param"]
+        self.num_output_group = int(self.learner_model_shape["num_class"])
+        self.num_feature = int(self.learner_model_shape["num_feature"])
+        
+        self.register_buffer("base_score", torch.Tensor([float(self.learner_model_shape["base_score"])]))
+        # self.base_score = float(self.learner_model_shape["base_score"])
+        
+        # A field encoding which output group a tree belongs
+        self.tree_info = model["learner"]["gradient_booster"]["model"]["tree_info"]
+
+        model_shape: ParamT = model["learner"]["gradient_booster"]["model"][
+            "gbtree_model_param"
+        ]
+
+        # JSON representation of trees
+        j_trees = model["learner"]["gradient_booster"]["model"]["trees"]
+
+        # Load the trees
+        self.num_trees = int(model_shape["num_trees"])
+        self.leaf_size = int(model_shape["size_leaf_vector"])
+        # Right now XGBoost doesn't support vector leaf yet
+        assert self.leaf_size == 0, str(self.leaf_size)
+
+        trees = []
+        for i in range(self.num_trees):
+            tree: Dict[str, Any] = j_trees[i]
+            tree_id = int(tree["id"])
+            assert tree_id == i, (tree_id, i)
+            # - properties
+            left_children: List[int] = tree["left_children"]
+            right_children: List[int] = tree["right_children"]
+            parents: List[int] = tree["parents"]
+            split_conditions: List[float] = tree["split_conditions"]
+            split_indices: List[int] = tree["split_indices"]
+            # when ubjson is used, this is a byte array with each element as uint8
+            default_left = to_integers(tree["default_left"])
+
+            # - categorical features
+            # when ubjson is used, this is a byte array with each element as uint8
+            split_types = to_integers(tree["split_type"])
+            # categories for each node is stored in a CSR style storage with segment as
+            # the begin ptr and the `categories' as values.
+            cat_segments: List[int] = tree["categories_segments"]
+            cat_sizes: List[int] = tree["categories_sizes"]
+            # node index for categorical nodes
+            cat_nodes: List[int] = tree["categories_nodes"]
+            assert len(cat_segments) == len(cat_sizes) == len(cat_nodes)
+            cats = tree["categories"]
+            assert len(left_children) == len(split_types)
+
+            # The storage for categories is only defined for categorical nodes to
+            # prevent unnecessary overhead for numerical splits, we track the
+            # categorical node that are processed using a counter.
+            cat_cnt = 0
+            if cat_nodes:
+                last_cat_node = cat_nodes[cat_cnt]
+            else:
+                last_cat_node = -1
+            node_categories: List[List[int]] = []
+            for node_id in range(len(left_children)):
+                if node_id == last_cat_node:
+                    beg = cat_segments[cat_cnt]
+                    size = cat_sizes[cat_cnt]
+                    end = beg + size
+                    node_cats = cats[beg:end]
+                    # categories are unique for each node
+                    assert len(set(node_cats)) == len(node_cats)
+                    cat_cnt += 1
+                    if cat_cnt == len(cat_nodes):
+                        last_cat_node = -1  # continue to process the rest of the nodes
+                    else:
+                        last_cat_node = cat_nodes[cat_cnt]
+                    assert node_cats
+                    node_categories.append(node_cats)
+                else:
+                    # append an empty node, it's either a numerical node or a leaf.
+                    node_categories.append([])
+
+            # Stats
+            base_weights: List[float] = tree["base_weights"]
+            gain: List[float] = tree["loss_changes"]
+            cover: List[float] = tree["sum_hessian"]
+
+            # Construct a list of nodes that have complete information
+            n_layers = 0
+            node_id = 0
+            weights = []
+            biases = []
+            leaves = []
+                        
+            # K * sigmoid(Wx - b)
+            frontier = [node_id]
+            while not all([node_id == -1 for node_id in frontier]):
+                n_layers += 1
+                new_frontier = []
+                new_leaves = []
+                for i, node_id in enumerate(frontier):
+                    if node_id == -1:
+                        weight = torch.ones(self.num_feature) / self.num_feature
+                        bias = torch.zeros(1)
+                        leaf = torch.Tensor([leaves[(i - 1) // 2]])
+                    else:
+                        weight = torch.zeros(self.num_feature)
+                        weight[split_indices[node_id]] = 1.0
+                        bias = torch.Tensor([split_conditions[node_id]])
+                        leaf = torch.Tensor([split_conditions[node_id]])
+                    weights.append(weight)
+                    biases.append(bias)
+                    
+                    new_leaves.append(leaf)
+                    if left_children[node_id] > -1:
+                        new_frontier.append(left_children[node_id])
+                    else:
+                        new_frontier.append(-1)
+                        
+                    if right_children[node_id] > -1:
+                        new_frontier.append(right_children[node_id])
+                    else:
+                        new_frontier.append(-1)
+                        
+                frontier = new_frontier
+                leaves = new_leaves
+            
+            weights = torch.stack(weights)
+            biases = torch.cat(biases)
+            leaves = torch.cat(leaves)
+
+            trees.append({
+                'weights': weights,
+                'biases': biases,
+                'leaves': leaves,
+                'n_layers': n_layers
+            })
+        self.trees = trees
+    
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        
+        preds = torch.stack([
+            self.forward_tree(
+                input, 
+                tree['weights'], 
+                tree['biases'], 
+                tree['leaves'], 
+                tree['n_layers']) 
+            for tree in self.trees], dim=-1)
+        return torch.sum(preds, dim=-1) + self.base_score
+
+    def forward_tree(
+        self, 
+        input: torch.Tensor, 
+        weights: torch.Tensor, 
+        biases: torch.Tensor, 
+        leaves: torch.Tensor,
+        n_layers: int) -> torch.Tensor:
+        
+        batch_size = input.shape[0]
+        
+        if self.training:
+            condition = F.linear(input, weights, -biases)
+            condition = torch.cat([condition[:, :, None], torch.zeros_like(condition[:, :, None])], -1)
+            acts = F.gumbel_softmax(condition, hard=False)
+        else:
+            act = torch.heaviside(F.linear(input, weights, -biases), torch.Tensor([0]))
+            act_comp = 1 - act
+            acts = torch.stack([act_comp, act], dim=-1)
+        
+        out = acts[:, 0, :].view(batch_size, -1, 1)
+        for curr_layer in range(1, n_layers-1):
+            curr_acts = acts[:, 2 ** curr_layer - 1 : 2 ** (curr_layer + 1) - 1, :]
+            out = curr_acts * out
+            out = out.view(batch_size, -1, 1)
+        
+        out = out.view(batch_size, -1) * leaves
+        return torch.sum(out, dim=-1)
